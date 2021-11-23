4c60eaa115ddaf77688f9a5b0b8b4b721687b88c
Yaamiin <91520140+Yaamiin@users.noreply.github.com>
2021-11-23 19:41:09 +0300

    Update _main_.py

--- IyaadXmusic/_main_.py
+++ IyaadXmusic/_main_.py
@@ -19,7 +19,7 @@ import requests
 from pyrogram import Client as Bot
 
 from IyaadXmusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from IyaadXmusic.musiccall.callsmusic import run
+from DaisyXMusic.services.callsmusic.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
